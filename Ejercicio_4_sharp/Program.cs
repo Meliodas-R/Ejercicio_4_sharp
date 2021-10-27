db6e3f99da26b056a55b691d6d17d2366eab062e
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-27 13:06:04 +0200

    Agregada la operación para la conversión de grados Fahrenheit a Celsius y su muestre por pantalla.

--- Ejercicio_4_sharp/Program.cs
+++ Ejercicio_4_sharp/Program.cs
@@ -6,7 +6,12 @@ namespace Ejercicio_4_sharp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Introduzca una temperatura en grados Fahrenheit: ");
+            decimal fahrenheit = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado = (5m/9m) * (fahrenheit-32);
+
+            Console.WriteLine(fahrenheit + " grados Fahrenheit equivalen a " + Math.Round(resultado, 2) + " grados Celsius.");
         }
     }
 }
